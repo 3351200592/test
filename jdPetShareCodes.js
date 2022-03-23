6dbcd7eb9a09deae6be30ce14bb1b494d00dbcda
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2022-03-23 21:13:05 +0800

    Update jdPetShareCodes.js

--- jdPetShareCodes.js
+++ jdPetShareCodes.js
@@ -20,16 +20,16 @@ let PetShareCodes = [
 // 判断github action里面是否有东东萌宠互助码
 if (process.env.PETSHARECODES) {
   if (process.env.PETSHARECODES.indexOf('&') > -1) {
-    console.log(`您的东东萌宠互助码选择的是用&隔开\n`)
+    // console.log(`您的东东萌宠互助码选择的是用&隔开\n`)
     PetShareCodes = process.env.PETSHARECODES.split('&');
   } else if (process.env.PETSHARECODES.indexOf('\n') > -1) {
-    console.log(`您的东东萌宠互助码选择的是用换行隔开\n`)
+    // console.log(`您的东东萌宠互助码选择的是用换行隔开\n`)
     PetShareCodes = process.env.PETSHARECODES.split('\n');
   } else {
     PetShareCodes = process.env.PETSHARECODES.split();
   }
 } else {
-  console.log(`由于您环境变量(PETSHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+  // console.log(`由于您环境变量(PETSHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
 }
 for (let i = 0; i < PetShareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
