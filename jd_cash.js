b9ddbb95262fc05f3138b8b4a521c17b62bf436b
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2022-04-03 12:16:22 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -341,8 +341,7 @@ function getSign(functionId, body) {
         if (Authorization) options["headers"]["Authorization"] = Authorization
         $.post(options, async (err, resp, data) => {
             try {
-                 console.log(data)
-
+                // console.log(data)
                 let res = $.toObj(data, data)
                 if (typeof res === 'object' && res) {
                     if (res.code && res.code == 200 && res.data) {
