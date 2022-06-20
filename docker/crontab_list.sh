d530e00da8ad7872223778fa0252b07d31416ee3
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2022-06-20 12:21:28 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -26,9 +26,6 @@
 # 萌虎摇摇乐
 0 1,10,20 * * * node /scripts/jd_mhyyl.js >> /scripts/logs/jd_mhyyl.log 2>&1
 
-# 通天塔签到共建
-0 0,2 * * * node /scripts/jd_babel_sign.js >> /scripts/logs/jd_babel_sign.log 2>&1
-
 # 京东特价 翻翻乐
 32 0,6-23 * * * node /scripts/jd_jdtj_winner.js >> /scripts/logs/jd_jdtj_winner.log 2>&1
 
